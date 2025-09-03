ab2fe28a4f9fa03c2c030122f1f1709c56fafff7
rayna806 <LUORA2@OOCL.COM>
2025-09-03 20:04:12 +0800

    initializeItemDatabase()从ItemsLoader加载数据构建HashMap

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,8 +1,24 @@
 package pos.machine;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PosMachine {
+    private Map<String, Item> itemDatabase;
+
+    public PosMachine() {
+        initializeItemDatabase();
+    }
+
+    private void initializeItemDatabase() {
+        itemDatabase = new HashMap<>();
+        List<Item> allItems = ItemsLoader.loadAllItems();
+        for (Item item : allItems) {
+            itemDatabase.put(item.getBarcode(), item);
+        }
+    }
+
     public String printReceipt(List<String> barcodes) {
         return null;
     }
